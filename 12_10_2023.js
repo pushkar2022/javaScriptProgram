d99ec5a92085e552ad83e2e9d9766afc2fb8d095
deepak <deepak@kgmail.com>
2023-10-12 20:12:31 +0530

    k

---
+++ 12_10_2023.js
@@ -0,0 +1,50 @@
+//1)  a=25    b=30    which one is big number
+
+// let a=2500
+// let b=2500
+
+// if(a>b){
+// console.log("this number is big a",a)
+// }
+// else{
+   
+//   console.log(("this number is big a",b) 
+//   }/
+//2.//middle values print 
+
+// let a=5  
+// let b=10
+// for(a=6;a<10;a++){
+//     console.log("show number",a)
+// }
+
+//3.  all values sum
+////////////a=1  k=0
+
+// let a=4
+// let k=0
+// for(i=1;i<a+1;i++){
+//      k=k+i
+//     console.log("k",k,   "i",i)
+// }
+// console.log("All values sum",k)/
+
+
+//4. postive negitive
+
+// let a=-6
+
+// if(a>0){
+//     console.log("postive")
+// }
+// else{
+//     console.log("nigetive")
+// }
+
+
+
+
+
+
+
+
