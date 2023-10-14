418c7e83578544683a4d24b09147ce68cd05e43f
deepak <deepak@kgmail.com>
2023-10-14 19:11:43 +0530

    start program

---
+++ 14_10_2023.js
@@ -0,0 +1,47 @@
+//1 add tow number
+a=10
+b=20
+console.log("sum to number",a+b)
+//2 Area of a reactangle
+let l=10
+let c=20
+
+console.log(l*c)
+//3 Chack if a number is even and odd
+let e=11
+
+if(e%2==0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+//4 find the maximum number
+let m=30
+let n=20
+
+if(m>n){
+    console.log("big number",m)
+}else{
+   console.log("big number",n) 
+}
+
+//Generate table:
+let t=21
+
+for(i=1;i<11;i++){
+    console.log("table",i*t)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
